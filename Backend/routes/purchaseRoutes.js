4232d071d1f13cdc947e10abdc5962a617acf6fe
AbsoluteChangeman <nathantionardi@gmail.com>
2025-07-06 00:23:22 +1000

    Update purchase route

--- Backend/routes/purchaseRoutes.js
+++ Backend/routes/purchaseRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const purchaseController = require()
+const purchaseController = require('../controller/purchaseController.js')
 
 // get all purchases
 router.get('/', purchaseController.getAllPurchases);
