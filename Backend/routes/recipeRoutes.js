61b2d18f403064e90442815af2d6e26624793acf
AbsoluteChangeman <nathantionardi@gmail.com>
2025-07-06 00:49:11 +1000

    Created recipe routes
    created recipe routes handling all operations

---
+++ Backend/routes/recipeRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const recipeController = require('../controllers/recipeController');
+
+// Get all recipes
+router.get('/', recipeController.getAllRecipes);
+
+// Create a new recipe
+router.post('/', recipeController.createRecipe);
+
+// Get a single recipe by its ID
+router.get('/:id', recipeController.getRecipeById);
+
+// Calculate the total cost of a single recipe
+router.get('/:id/cost', recipeController.getRecipeCost);
+
+// Update a recipe
+router.put('/:id', recipeController.updateRecipe);
+
+// Delete a recipe
+router.delete('/:id', recipeController.deleteRecipe);
+
+module.exports = router;
