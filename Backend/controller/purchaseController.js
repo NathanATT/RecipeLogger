bb74ef485d616a48b380cf91688e0673db7473b2
AbsoluteChangeman <nathantionardi@gmail.com>
2025-07-05 23:31:11 +1000

    Created purchase controller
    created purchase controller to handle new purchases and grab all purchases

---
+++ Backend/controller/purchaseController.js
@@ -0,0 +1,23 @@
+const purchaseQuery = require('../queries/purchaseQuery.js');
+
+module.exports = {
+    getAllPurchases : async (req, res) => {
+        try {
+            const purchases = await purchaseQuery.findAllPurchases();
+            res.status(200).json(purchases);
+        } catch (error) {
+            res.status(500).json({message: 'Error fetchiing purchases.', error: error.message})
+        }
+    },
+    logNewPurchase : async (req, res) => {
+        try {
+            const purchaseData = req.body;
+            const result = await purchaseQuery.createPurchaseAndUpdateIngredient(purchaseData);
+            res.status(201).json({
+                message: 'Purchase logged successfully', data: result
+            });
+        } catch (error) {
+            res.status(error.statusCode || 500).json({message: 'Error logging purchase.', error: error.message});
+        }
+    },
+}
